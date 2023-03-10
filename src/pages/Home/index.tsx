961493269a39c63a1e440d922fb2118e8f89c610
Kayo.io <kayorenatocontato@gmail.com>
2023-03-10 00:54:00 -0300

    chore: add style home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,58 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './style'
+
 export function Home() {
   return (
-    <div>
-      <h2>Home</h2>
-    </div>
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task"> I'll do ... </label>
+          <TaskInput
+            id="task"
+            type="text"
+            placeholder="something"
+            list="task-suggestion"
+          />
+
+          <datalist id="task-suggestion">
+            <option value="task 1">Task 1</option>
+            <option value="task 2">Task 2</option>
+            <option value="task 3">Task 3</option>
+          </datalist>
+
+          <label htmlFor="minutesAmount">for</label>
+          <MinutesInput
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+          <span>minutes</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Start
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
   )
 }
---
+++ src/pages/Home/style.ts
@@ -0,0 +1,128 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.main`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3.5rem;
+  }
+`
+
+export const FormContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
+  color: ${(props) => props.theme['gray-100']};
+
+  font-size: 1.125rem;
+  font-weight: bold;
+  flex-wrap: wrap;
+`
+
+const BaseInput = styled.input`
+  background: ${(props) => props.theme['gray-700']};
+  height: 2.5rem;
+  padding: 0.5rem 1rem;
+
+  border: none;
+  border-bottom: 2px solid ${(props) => props.theme['green-700']};
+
+  font-size: inherit;
+  font-weight: inherit;
+  color: ${(props) => props.theme.white};
+
+  &:focus {
+    background: ${(props) => props.theme['gray-700']};
+    border: 1px solid ${(props) => props.theme['green-700']};
+    box-shadow: none;
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-300']};
+    opacity: 0.5;
+  }
+`
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
+`
+
+export const MinutesInput = styled(BaseInput)`
+  width: 5rem;
+`
+
+export const CountdownContainer = styled.div`
+  font-family: 'Roboto Mono', monospace;
+  font-size: 10rem;
+  line-height: 8rem;
+  color: ${(props) => props.theme['gray-100']};
+
+  display: flex;
+  gap: 1rem;
+
+  span {
+    background: ${(props) => props.theme['gray-700']};
+    padding: 2rem 1rem;
+    border-radius: 8px;
+  }
+`
+
+export const Separator = styled.div`
+  padding: 2rem 0;
+  color: ${(props) => props.theme['green-500']};
+
+  width: 4ren;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+`
+
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  padding: 1rem;
+  border: none;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+
+  background: ${(props) => props.theme['green-300']};
+  color: ${(props) => props.theme.white};
+
+  font-size: 1.125rem;
+  font-weight: bold;
+
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  outline: none;
+
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.7;
+  }
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['green-500']};
+  }
+
+  &:not(:disabled):active {
+    background: ${(props) => props.theme['green-700']};
+  }
+`
